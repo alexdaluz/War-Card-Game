3a6a7d77482b3ba4de21ed4b1b0b5bd930a6b3bc
Alexander Da Luz <alexdaluz@me.com>
2025-10-29 11:31:20 -0500

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Alexander Da Luz on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
